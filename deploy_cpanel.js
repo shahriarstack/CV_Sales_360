be1fef4e34c415256cc96e4efc04030b16b5c44d
Motor-Sr.Executive <Shahriar.Imon@aci-bd.com>
2026-07-13 20:49:19 +0600

    Implement Manual Delivery Verification Approval and Comments Flow

--- deploy_cpanel.js
+++ deploy_cpanel.js
@@ -197,6 +197,7 @@ async function run() {
         console.log("[5/5] Preparing files for upload...");
         
         const filesToUpload = [
+            { name: 'inspect_emi.php', path: path.join(__dirname, 'inspect_emi.php') },
             { name: 'index.html', path: path.join(__dirname, 'index.html') },
             { name: 'api.php', path: path.join(__dirname, 'api.php') },
             { name: 'cpanel_setup.sql', path: path.join(__dirname, 'cpanel_setup.sql') }
